1206d0311e8f57d9f52bec04b2df9052480d1747
Nishant Mishra <nishonit2@gmail.com>
2018-05-08 18:29:35 +0530

    delete

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -9,30 +9,30 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     console.log('Connected to Mongodb server');
 
    //deleteMany
-   //  db.collection('Todos').deleteMany({text: "Learn Node js"}).then((result) => {
-   //     console.log(result);
-   //  });
+    db.collection('Todos').deleteMany({text: "Learn Node js"}).then((result) => {
+       console.log(result);
+    });
 
     //deleteOne
-    // db.collection('Todos').deleteOne({text: "Learn"}).then((result) => {
-    //     console.log(result);
-    // });
+    db.collection('Todos').deleteOne({text: "Learn"}).then((result) => {
+        console.log(result);
+    });
 
     //findOneAndDelete
-    //
-    // db.collection('Todos').findOneAndDelete({text: "Learn"}).then((result) => {
-    //     console.log(result);
-    // });
+
+    db.collection('Todos').findOneAndDelete({text: "Learn"}).then((result) => {
+        console.log(result);
+    });
 
     //deleteOne from user
-    //
-    //  db.collection('User').findOneAndDelete({
-    //     _id: new ObjectID("5af176cf73a9730980543298")
-    //  }).then((result) => {
-    //     console.log(result);
-    //  });
+
+     db.collection('User').findOneAndDelete({
+        _id: new ObjectID("5af176cf73a9730980543298")
+     }).then((result) => {
+        console.log(result);
+     });
 
 
 
-    //db.close();
+    db.close();
 });
