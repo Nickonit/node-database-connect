a94ff0dbed68880f9e04cb96d26b7a008608c7a6
Nishant Mishra <nishonit2@gmail.com>
2018-05-10 04:15:31 +0530

    Modified init

---
+++ server/server.js
@@ -0,0 +1,69 @@
+//External Module
+let express = require('express');
+let bodyParser = require('body-parser');
+
+//Files
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/User');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+//setting up the route
+app.post('/todos',(req, res) =>{
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) =>{
+        res.send(doc);
+    },(e) =>{
+        res.status(400).send(e);
+    });
+});
+
+
+
+
+app.listen(3000,() => {
+   console.log('server is running on port 3000');
+});
+
+
+
+
+// let newTodo = new Todo({
+//     text: 'Cook Dinner'
+// });
+//
+//
+// newTodo.save().then((doc) => {
+//     console.log('Saved Todo', doc);
+// },(e) => {
+//    console.log('unable to save todo');
+// });
+
+// let otherTodo = new Todo({
+//     text: 'Feed the cat',
+//     completed: true,
+//     completeAt: 123
+// });
+//
+// otherTodo.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//     console.log('unable to save todo');
+// });
+
+//
+//     let user1 = new User({
+//
+//     });
+//
+//     user1.save().then((doc) => {
+//         console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//         console.log('unable to save user',e);
+//     });
