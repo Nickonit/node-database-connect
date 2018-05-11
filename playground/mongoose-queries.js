8891808bf430434c33df285d802a2533b8f7dbe2
Nishant Mishra <nishonit2@gmail.com>
2018-05-12 00:39:54 +0530

    Check the Id

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,38 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const{Todo} = require('./../server/models/todo');
+const{User} = require('./../server/models/user');
+
+let todoid = '5af4cc02d6b8968825ab86e0';
+ let userid = '5af2bb143c7e99d4094a9fbb';
+
+if(!ObjectID.isValid(todoid && userid)){
+    console.log('ID not valid');
+}
+
+Todo.find({
+    _id: todoid
+}).then((todos) =>{
+  console.log('todos', todos);
+});
+
+Todo.findOne({
+    _id: todoid
+}).then((todo) =>{
+    console.log('todo', todo);
+});
+
+Todo.findById(todoid).then((todo) =>{
+    if(!todo){
+        return console.log('Id not Find');
+    }
+    console.log('todo by id', todo);
+}).catch((e) =>console.log(e));
+
+User.findById(userid).then((user) =>{
+    if(!user) {
+        return console.log('ID not found');
+    }
+
+    console.log('User ID', user);
+}).catch((e) => console.log(e));
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 //library imports
 let express = require('express');
 let bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 //local imports with Destructuring
 let {mongoose} = require('./db/mongoose');
@@ -32,6 +33,19 @@ app.get('/todos',(req,res) =>{
    });
 });
 
+app.get('/todos/:id',(req, res) =>{
+    let id =req.params.id;
+    if(!ObjectID.isValid(id)){
+       return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) =>{
+       if(!todo){
+        return res.status(404).send();
+       }
+       res.send({todo});
+    }).catch((e)=> res.status(400).send());
+});
 
 app.listen(3000,() => {
    console.log('server is running on port 3000');
